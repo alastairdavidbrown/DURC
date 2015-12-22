a3a4be244a3bc8d034744db166de0c23c8a0a6d0
Alastair Brown <alastair.brown@laterooms.com>
2015-12-22 16:57:32 +0000

    change server main

---
+++ durc-server.js
@@ -0,0 +1,42 @@
+// app.js - load dependencies and listen!
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var passport = require('passport');
+var util = require('util')
+var session = require('express-session');
+var flash = require('connect-flash');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// set the view engine to ejs, cookie parser
+app.set('view engine', 'ejs');		// View engine
+app.use(express.static('public'));	// Static folder
+app.use(cookieParser());			
+app.use(bodyParser.json());			// rbody parser
+app.use(bodyParser.urlencoded({
+	extended: true
+})); 								// for parsing application/x-www-form-urlencoded
+
+
+//secret: 'span0dd2',
+app.use(session({
+	secret: 'durc150607',
+	saveUninitialized: true,
+	resave: true
+}));
+
+app.use(passport.initialize());		// passport authentication
+app.use(passport.session());
+app.use(flash());					// flash for messaging
+app.use(morgan('combined'));		// logger
+
+console.log("connecting to:" + process.env.DB_URL_APP);
+mongoose.connect(process.env.DB_URL_APP); 					// connect to our database
+
+require('./config/passport')(passport); 			// pass passport for configuration
+require('./app/routes.js')(app, passport, express); 			// load our routes and pass in our app and fully configured passport
+
+console.log("Starting durc port:" + process.env.PORT)
+app.listen(process.env.PORT || 3000);									
